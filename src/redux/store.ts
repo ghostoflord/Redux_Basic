47b8bb674c4a270ebdee660ef71c6ebaa9a289e2
ghostoflord <ghost2892003zzz@gmail.com>
2024-11-27 21:46:55 +0700

    update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import counterReducer from './counter/counterSlice'
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        ghost: counterReducer
+    },
 })
 
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
