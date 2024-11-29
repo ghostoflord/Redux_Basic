4df48cb4f36fdf5fef4f687fb86f87b40e9ef0aa
ghostoflord <ghost2892003zzz@gmail.com>
2024-11-29 16:45:03 +0700

    update set boostrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { decrease, increment } from './redux/counter/counterSlice'
 import { useAppDispatch, useAppSelector } from './redux/hooks'
 
